1641314f0a97400b634c480bb8babd9c8c42e5a6
venky216 <venkyroyal216@gmail.com>
2021-04-30 20:36:18 +0530

    first commit

--- packages/ler3-com-react/dist/components.js
+++ packages/ler3-com-react/dist/components.js
@@ -2,11 +2,8 @@
 /* tslint:disable */
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
-
-import type { JSX } from 'ler3-com';
-
 import { applyPolyfills, defineCustomElements } from 'ler3-com/loader';
-
 applyPolyfills().then(() => defineCustomElements());
-export const AlCard = /*@__PURE__*/createReactComponent<JSX.AlCard, HTMLAlCardElement>('al-card');
-export const AlCounter = /*@__PURE__*/createReactComponent<JSX.AlCounter, HTMLAlCounterElement>('al-counter');
+export const AlCard = /*@__PURE__*/ createReactComponent('al-card');
+export const AlCounter = /*@__PURE__*/ createReactComponent('al-counter');
+//# sourceMappingURL=components.js.map
---
+++ packages/ler3-com/src/components/al-counter/readme.md
@@ -0,0 +1,25 @@
+# al-counter
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type     | Default |
+| -------- | --------- | ----------- | -------- | ------- |
+| `max`    | `max`     |             | `number` | `null`  |
+| `min`    | `min`     |             | `number` | `null`  |
+
+
+## Events
+
+| Event      | Description | Type                  |
+| ---------- | ----------- | --------------------- |
+| `alChange` |             | `CustomEvent<number>` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
